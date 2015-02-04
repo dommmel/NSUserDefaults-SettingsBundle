a08c0fdc08103e58385c43d6b30d0c700cff33b0
Dommmel <dommmel@gmail.com>
2015-02-04 16:46:59 +0100

    init

---
+++ NSUserDefaults+SettingsBundle.h
@@ -0,0 +1,13 @@
+//
+//  NSUserDefaults+SettingsBundle.h
+//  Wickel
+//
+//  Created by Dominik on 04/02/15.
+//  Copyright (c) 2015 Dominik Heier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSUserDefaults (SettingsBundle)
+- (void)registerDefaultsFromSettingsBundle;
+@end
